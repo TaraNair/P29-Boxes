472928fc745f0fdfbd42b8cdd3f0f98c7b03fba9
TaraNair <64844272+TaraNair@users.noreply.github.com>
2020-09-20 18:10:24 +0530

    Added Files

---
+++ p5.play-boilerplate-master/Ball.js
@@ -0,0 +1,26 @@
+class Ball{
+    constructor(x, y, width){
+        var options = {
+            'isStatic' : false,
+            'restitution' : 0,
+            'friction' : 1,
+            'density' : 1.2
+        }
+        this.body = Bodies.circle(x, y, width/3, options);
+        this.width = width;
+        this.radius = width/3;
+    
+        World.add(world, this.body);
+    }
+  
+    display() {
+        var pos = this.body.position;
+        push();
+        var pos =this.body.position;
+        ellipseMode(CENTER);
+        fill(255, 255, 150);
+        ellipse(pos.x, pos.y, this.width, this.height);
+        pop();
+    }
+  }
+    
